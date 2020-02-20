753bfe5697f67f947eebe21a15ddeec53550d2f2
Eddy-Martinez <61172190+Eddy-Martinez@users.noreply.github.com>
2020-02-20 13:48:56 -0600

    Update ALARMA.py

--- ALARMA.py
+++ ALARMA.py
@@ -1,18 +1,18 @@
-global bandera
+global alarma
 class contraseña:
         def __init__(self,contaseña):
                 self.contraseña=contraseña
 
         def pregunta(self):
-                global bandera
-                bandera = 1
+                global alarma
+                alarma = 1
                 contraseña=1234
                 real=0
                 while (real != contraseña):
                         real=int(input("Ingrese contraseña\t"))
                         if(real==contraseña):
                                 print ("Alarma desactivada\n")
-                                bandera = 0
+                                alarma = 0
                         else:
                                 print("Contraseña incorrecta\n")
 class activa:
@@ -20,15 +20,15 @@ class activa:
                 self.activa=activa
 
         def pregunta(self):
-                global bandera
+                global alarma
                 respuesta='N'
                 while (respuesta != 'S'):
                         respuesta=input("¿Activar alarma?\n")
                 print("Alarma activada\n")
-                bandera = 1
+                alarma = 1
                 
 activa.pregunta('S')
-while (bandera == 1):
+while (alarma == 1):
         contraseña.pregunta(1234)
-        while (bandera == 0):
+        while (alarma == 0):
                 activa.pregunta('S')       
